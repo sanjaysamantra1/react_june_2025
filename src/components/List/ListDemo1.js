1deda75984f3debfcb334c45d1e5b6e16ce6f975
Sanjay Samantra <sanjaysamantra1@gmail.com>
2025-06-25 10:15:31 +0530

    List

--- src/components/List/ListDemo1.js
+++ src/components/List/ListDemo1.js
@@ -7,5 +7,20 @@ export default function ListDemo1() {
                 return <li key={ind}>{stateName}</li>
             })}
         </ol>
+
+        <h3>State Names in Dropdown</h3>
+        <select>
+            {stateNames.map((stateName, ind) => {
+                return <option key={ind}>{stateName}</option>
+            })}
+        </select>
+
+        <h3>State Names in Datalist</h3>
+        <input list="stateNames" name="" id=""></input>
+        <datalist id="stateNames">
+            {stateNames.map((stateName, ind) => {
+                return <option key={ind}>{stateName}</option>
+            })}
+        </datalist>
     </>
 }
--- src/components/body/Body.js
+++ src/components/body/Body.js
@@ -5,6 +5,7 @@ import Greet from '../greet/Greet';
 import MyModal from '../myModal/MyModal';
 import ConditionalDemo1 from '../conditional_rendering/ConditionalDemo1';
 import ListDemo1 from '../List/ListDemo1';
+import ListDemo2 from '../List/ListDemo2';
 
 export default function Body() {
   let msg1 = 'Good Afternoon';
@@ -20,7 +21,8 @@ export default function Body() {
         <h3>This Content is from parent component</h3>
       </Greet> */}
       {/* <ConditionalDemo1 /> */}
-      <ListDemo1 />
+      {/* <ListDemo1 /> */}
+      <ListDemo2 />
     </div>
   )
 }
